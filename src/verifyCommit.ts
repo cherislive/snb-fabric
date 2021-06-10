87b2aee04d11ca57bac37c9d5ef03fef29ebf56e
unknown <zhangxiaoyang-jk@360shuke.com>
2021-06-10 17:17:53 +0800

    chalk color

--- src/verifyCommit.ts
+++ src/verifyCommit.ts
@@ -7,6 +7,10 @@ const osLocale = require('os-locale');
 const msgPath = process.env.GIT_PARAMS || process.env.HUSKY_GIT_PARAMS;
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 
+
+chalk.enabled = true;
+chalk.level = 2;
+
 const commitRE =
   /^(((\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55]) )?(revert: )?(feat|fix|docs|UI|refactor|⚡perf|workflow|build|CI|typos|chore|tests|types|wip|release|dep|locale)(\(.+\))?: .{1,50}/;
 
